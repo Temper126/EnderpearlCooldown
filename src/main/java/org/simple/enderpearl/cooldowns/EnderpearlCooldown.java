f79e5a4a511b33c8e85345111f10c369be7a7bbf
Temper126 <42579059+Temper126@users.noreply.github.com>
2022-10-06 08:07:36 -0400

    Redone the code!

--- src/main/java/org/simple/enderpearl/cooldowns/EnderpearlCooldown.java
+++ src/main/java/org/simple/enderpearl/cooldowns/EnderpearlCooldown.java
@@ -11,11 +11,15 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.projectiles.ProjectileSource;
+import org.simple.actionbar.ActionBarAPI;
+import org.simple.cooldown.PlayerCooldownMap;
 import org.simple.enderpearl.EnderpearlPlugin;
 import org.simple.utils.SimpleUtil;
 
 public class EnderpearlCooldown implements Listener {
 	private final EnderpearlPlugin plugin;
+	public static final String COOLDOWN_ID = "enderpearl";
+	public PlayerCooldownMap playerCooldownMap = new PlayerCooldownMap("enderpearl");
 
 	public EnderpearlCooldown(EnderpearlPlugin plugin) {
 		this.plugin = plugin;
@@ -26,10 +30,14 @@ public class EnderpearlCooldown implements Listener {
 	)
 	public void on(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.ENDER_PEARL && this.plugin.getCooldownHandler().isOnCooldown(event.getPlayer(), "Ender pearl")) {
-			SimpleUtil.sendMessage(event.getPlayer(), EnderpearlPlugin.PEARL_MESSAGE);
-			event.setCancelled(true);
-			player.updateInventory();
+		if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.ENDER_PEARL) {
+			if (this.playerCooldownMap.isOnCooldown(player, COOLDOWN_ID)) {
+				SimpleUtil.sendMessage(player, EnderpearlPlugin.PEARL_MESSAGE);
+				event.setCancelled(true);
+			} else {
+				ActionBarAPI.sendActionBar(this.plugin, player, "Enderpearl", EnderpearlPlugin.PEARL_COOLDOWN);
+				this.playerCooldownMap.add(player, EnderpearlPlugin.PEARL_COOLDOWN);
+			}
 		}
 
 	}
@@ -45,7 +53,7 @@ public class EnderpearlCooldown implements Listener {
 			ProjectileSource source = enderPearl.getShooter();
 			if (source instanceof Player) {
 				Player shooter = (Player) source;
-				this.plugin.getCooldownHandler().addCooldown(shooter, "Ender pearl", EnderpearlPlugin.PEARL_COOLDOWN);
+				this.playerCooldownMap.add(shooter, COOLDOWN_ID, EnderpearlPlugin.PEARL_COOLDOWN);
 			}
 		}
 
